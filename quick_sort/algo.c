2067647b74855516344e652eb20c2ff9c4f3b484
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2025-02-23 19:23:42 +0100

    feat: quick sort impl

--- quick_sort/algo.c
+++ quick_sort/algo.c
@@ -1,9 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int *quicksort(int *arr, int len) {}
+int partition(int *arr, int start, int end) {
+  int pivot = arr[end];
+  int i = start - 1;
+  int temp = 0;
+  for (int j = start; j < end; j++) {
+    if (arr[j] <= pivot) {
+      i++;
+      temp = arr[j];
+      arr[i] = arr[j];
+      arr[j] = temp;
+    }
+  }
+  temp = arr[i + 1];
+  arr[i + 1] = arr[end];
+  arr[end] = temp;
+  return i + 1;
+}
+
+void quicksort(int *arr, int start, int end) {
+  if (start < end) {
+    int q = partition(arr, start, end);
+    quicksort(arr, start, q - 1);
+    quicksort(arr, q + 1, end);
+  }
+}
 
 int main() {
-  printf("hello world");
+  int arr[10] = {1, 2, 3, 45, 234, 92, 120, 643, 23, 93};
+  int len = sizeof(arr) / sizeof(arr[0]);
+  printf("Unsorted array: \n");
+  for (int i = 0; i < len; i++) {
+    printf("%d\n", arr[i]);
+  }
+
+  printf("Sorted array: \n");
+
+  quicksort(arr, 0, 9);
+  for (int i = 0; i < len; i++) {
+    printf("%d\n", arr[i]);
+  }
+
   return 0;
 }
